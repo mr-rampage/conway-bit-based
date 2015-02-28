66cd1663619a48645c533ee72c33a28fe76df8f5
mr. rampage <readonly@che.wbac.ca>
2015-02-28 11:02:25 -0500

    Removed need for converting index

--- src/ca/wbac/conway/Life.java
+++ src/ca/wbac/conway/Life.java
@@ -4,90 +4,77 @@ public class Life {
     private final int height;
     private final int width;
     private final int world;
-    
-    int getIndex(int x, int y, int w) {
-        return x + y * w;
-    }
 
-    boolean dies(int p) {
+    boolean dies(final int p) {
         return (p < 2 || p > 3);
     }
 
-    boolean grow(int p) {
+    boolean grow(final int p) {
         return p == 3;
     }
-
-    int maskArea(int x, int y, int w) {  	
-        int mask = 0;
-        for (int dy = -1; dy <= 1; dy++) {
-        	if ((y + dy) > -1) {
-	        	for (int dx = -1; dx <= 1; dx++) {
-	                if ((x + dx) < w && (x + dx) > -1) {
-	                    mask |= 1 << getIndex(x + dx, y + dy, w);
-	                }
-	            }
-        	}
-        }
-        return mask;
+    
+    int maskArea(final int index, final int w, final int size) {
+    	int mask = 0;
+    	for (int i = -1; i <= 1; i++) {
+    		int pos = index + i * w;
+    		int min = Math.floorDiv(pos--, w) * w;
+    		int max = min + w;
+    		if (max > 0) {
+	    		for (int j = 0; j <= 2; j++) {
+	    			int cell = pos + j;
+		    		if (cell >= 0 && cell < size && 
+		    				cell >= min && cell < max ) {
+		    	    	mask ^= 1 << cell;
+		    		}
+	    		}
+    		}
+    	}
+    	
+    	return mask;
     }
     
     int countBits(final int value) {
         int count = 0;
         int buffer = value;
         while (buffer > 0) {
-            if ((buffer & 1) == 1) {
-                count++;
-            }
+        	count += buffer & 1;
             buffer >>= 1;
         }
         return count;
     }
     
-    int isAlive(int world, int x, int y, int w) {
-    	int index = getIndex(x, y, w);
+    int bitAt(final int world, final int index) {
     	return (world & 1 << index) >> index;
     }
-
-    int countNeighbours(int world, int x, int y, int w) {
-        int mask = maskArea(x, y, w);
-        return countBits(world & mask) - isAlive(world, x, y, w);
+    
+    int countNeighbours(final int world, final int index, final int w, final int s) {
+    	int mask = maskArea(index, w, s);
+    	return countBits(world & mask) - bitAt(world, index);
     }
     
-    int next(int world, int columns, int rows, int width) {
+    int next(final int world, final int w, final int h) {
     	int newWorld = world;
-		for (int y = 0; y < rows; y++) {
-			for (int x = 0; x < columns; x++) {
-    			int population = countNeighbours(world, x, y, width);
-    			int index = getIndex(x, y, width);
-    			if (isAlive(world, x, y, width) == 1) {
-    				if (dies(population)) {
-    					newWorld ^= (1 << index);
-    				}
-    			} else if (grow(population)) {
-					newWorld |= 1 << index;
-    			}
-    		}
+    	int index = 0;
+    	int size = w * h;
+    	while (size > 0 && index < size) {
+			int population = countNeighbours(world, index, w, size);
+			if (bitAt(world, index) == 1) {
+				if (dies(population)) {
+					newWorld ^= 1 << index;
+				}
+			} else if (grow(population)) {
+				newWorld ^= 1 << index;
+			}
+			index++;
     	}
     	return newWorld;
-    }
-    
-    void render(int world, int columns, int rows, int width) {
-		for (int y = 0; y < rows; y++) {
-			for (int x = 0; x < columns; x++) {
-    			int index = getIndex(x, y, width);
-    			System.out.print((((world >> index) & 1) == 1 ? "*" : "."));
-    		}
-			System.out.println();
-    	}
-		System.out.println();
     }
     
     public void run(final int generations) {
     	int g = generations;
     	int w = this.world;
     	while (g-- > 0) {
-    		w = this.next(w, this.width, this.height, this.width);
-    		this.render(w, this.width, this.height, this.width);
+    		w = this.next(w, this.width, this.height);
     	}
     }
 
--- test/ca/wbac/conway/LifeTest.java
+++ test/ca/wbac/conway/LifeTest.java
@@ -14,12 +14,6 @@ public class LifeTest {
     public void setUp() {
         fixture = new Life(0b011100000000000, 5, 5);
     }
-
-    @Test
-    public void shouldGetIndexFromCoordinate() {
-        assertThat(fixture.getIndex(0, 1, 4), is(4));
-        assertThat(fixture.getIndex(3, 1, 4), is(7));
-    }
     
     @Test
     public void shouldDieIfUnderOrOverPopulated() {
@@ -39,33 +33,22 @@ public class LifeTest {
     }
 
     @Test
-    public void shouldMaskAnArea() {
-    	assertThat(fixture.maskArea(0, 0, 3), is(0b11011));
-        assertThat(fixture.maskArea(1, 1, 3), is(0b111111111));
-        assertThat(fixture.maskArea(2, 1, 4), is(0b111011101110));
-        assertThat(fixture.maskArea(2, 2, 4), is(0b1110111011100000));
-        assertThat(fixture.maskArea(0, 2, 4), is(0b11001100110000));
-        assertThat(fixture.maskArea(2, 0, 4), is(0b11101110));
-        assertThat(fixture.maskArea(2, 0, 3), is(0b110110));
-    }
-    
-    @Test
     public void shouldReturnTrueIfAlive() {
-    	assertThat(fixture.isAlive(1, 0, 0, 3), is(1));
-    	assertThat(fixture.isAlive(0, 1, 0, 3), is(0));
+    	assertThat(fixture.bitAt(1, 0), is(1));
+    	assertThat(fixture.bitAt(0, 1), is(0));
     }
     
     @Test
     public void shouldCountNeighbours() {
-    	assertThat(fixture.countNeighbours(0b1011, 0, 0, 3), is(2));
-    	assertThat(fixture.countNeighbours(0b1011, 2, 0, 3), is(1));
-    	assertThat(fixture.countNeighbours(0, 2, 2, 3), is(0));
+    	assertThat(fixture.countNeighbours(0b1011, 0, 3, 9), is(2));
+    	assertThat(fixture.countNeighbours(0b1011, 2, 3, 9), is(1));
+    	assertThat(fixture.countNeighbours(0, 8, 3, 9), is(0));
     }
     
     @Test
     public void shouldRotate() {
-    	assertThat(fixture.next(0b010010010, 3, 3, 3), is(0b0000111000));
-    	assertThat(fixture.next(0b0000111000, 3, 3, 3), is(0b010010010));
+    	assertThat(fixture.next(0b010010010, 3, 3), is(0b0000111000));
+    	assertThat(fixture.next(0b0000111000, 3, 3), is(0b010010010));
     }
     
     @Test
@@ -73,4 +56,15 @@ public class LifeTest {
     	fixture.run(10);
     }
     
+    @Test
+    public void shouldMaskAreaByIndex() {
+    	assertThat(fixture.maskArea(0, 3, 9), is(0b011011));
+    	assertThat(fixture.maskArea(4, 3, 9), is(0b111111111));
+    	assertThat(fixture.maskArea(0, 3, 9), is(0b11011));
+        assertThat(fixture.maskArea(6, 4, 16), is(0b111011101110));
+        assertThat(fixture.maskArea(10, 4, 16), is(0b1110111011100000));
+        assertThat(fixture.maskArea(8, 4, 16), is(0b11001100110000));
+        assertThat(fixture.maskArea(2, 4, 16), is(0b11101110));
+        assertThat(fixture.maskArea(2, 3, 9), is(0b110110));
+    }
 }
